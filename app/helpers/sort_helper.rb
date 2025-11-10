437bcc5fd16ed8999f8fb25360e5ad6b0a3239c8
Marc Beaupré <hrdwdmrbl@gmail.com>
2025-11-10 11:07:44 -0500

    Fix: Default ordering of columns in Problem table
    Issue: Default order was always asc which is often not what is wanted.
    Solution: Hard-code some ordering for UX

--- app/helpers/sort_helper.rb
+++ app/helpers/sort_helper.rb
@@ -3,7 +3,25 @@ module SortHelper
   def link_for_sort(name, field = nil)
     field ||= name.underscore
     current = (params_sort == field)
-    order = (current && (params_order == "asc")) ? "desc" : "asc"
+    order = if current && params_order == "desc"
+      # Opposite of current order
+      "asc"
+    elsif current && params_order == "asc"
+      # Opposite of current order
+      "desc"
+    elsif field == "message" || field == "app" || field == "environment"
+      # Alphabetical order
+      "asc"
+    elsif field == "last_notice_at"
+      # Date order - Most recent first
+      "desc"
+    elsif field == "count"
+      # Count order - Most frequent first
+      "desc"
+    else
+      # Default order - Alphabetical / numeric order
+      "asc"
+    end
     url = request.path + "?sort=#{field}&order=#{order}"
     url += "&all_errs=true" if all_errs
     options = {}
